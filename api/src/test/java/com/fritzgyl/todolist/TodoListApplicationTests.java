b8dbd10b598a7a119004cf0b87583e1e984c0010
Lubin Fritzgy <fritzgy.lubin@etudiant.univ-rennes1.fr>
2022-05-08 21:02:26 +0200

    first commit

---
+++ api/src/test/java/com/fritzgyl/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fritzgyl.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
